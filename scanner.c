e12448ff8f813687a821c42124c6016ec9cd1407
tommygood <tommy50508@gmail.com>
2024-05-10 17:02:29 +0800

    state of simple scanner

---
+++ scanner.c
@@ -0,0 +1,393 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "search_opcode.c"
+#include <math.h>
+
+char ** split(char * s) {
+    // split the string with space and newline
+    int s_len = strlen(s);
+    char ** s_arr = calloc(s_len, sizeof(char *));
+    char * temp = calloc(s_len, sizeof(char));
+    int index = 0;
+    
+    for (int i = 0;i < s_len;i++) {
+        if (s[i] != ' ' && s[i] != '\t') {
+	    //printf("%d %c is not space, but is %d\n", i, s[i], s[i] == '\0');
+            // add char into temp if not counter the space or newline
+            strncat(temp, &s[i], 1); // append the char to string
+        }
+        else {
+	    //printf("%d %c is space, but is %d\n", i, s[i], s[i] == '\0');
+	    if (temp[0] != '\0') {
+            	// place temp into array and clear the temp
+            	s_arr[index++] = strdup(temp);
+	    }
+            // clear the temp
+            temp[0] = '\0';
+        }
+    }
+    if (temp[0] != '\0') {
+        // place temp into array and clear the temp
+        s_arr[index++] = strdup(temp);
+    }
+    //for (int i = 0;i < s_len;i++)
+    //	printf("s arr %d : %s\n", i, s_arr[i]);
+    
+    return s_arr;
+}
+
+char * commaRemoveSpace(char * line) {
+    // remove a space behind the comma
+    int comma_index = -1;
+    for (int i = 0;i < strlen(line);i++) {
+	if (line[i] == ',') {
+	    comma_index = i; // which index is comma
+	    break;
+	}
+    }
+    // return origin string, if no comma
+    // remove a space behind comma, otherwise
+    if (comma_index == -1) {
+	return line;
+    }
+    else {
+	char * new_str = malloc((strlen(line)-1) * sizeof(char));
+	int index = 0;
+	for (int i = 0;i < strlen(line);i++) {
+	    if (i != comma_index + 1) {
+		new_str[index++] = line[i];
+	    }
+	}
+	return new_str;
+    }
+}		    
+
+char * removeComment(char * line) {
+    // remove comments, which are behind the dot
+    char * n_str = calloc(strlen(line), sizeof(char));
+    n_str[0] = '\0';
+    for (int i = 0;i < strlen(line);i++) {
+	if (line[i] == '.') {
+	    break;
+	}
+	else {
+	    n_str[i] = line[i];
+	}
+    }
+
+    return n_str;
+}
+
+int isBasic(char * line) {
+    char * second_place = split(line)[1];
+    char * first_place = split(line)[0];
+    if (second_place == '\0') // avoid  
+	return 0;
+    if (strcmp(second_place, "START") == 0)
+	return 1;
+    else if (strcmp(first_place, "END") == 0)
+    	return 2;
+    else if (strcmp(second_place, "WORD") == 0)
+    	return 3;
+    else if (strcmp(second_place, "RESW") == 0)
+    	return 4;
+    else if (strcmp(second_place, "BYTE") == 0)
+    	return 5;
+    else if (strcmp(second_place, "RESB") == 0)
+    	return 6;
+    else 
+    	return 0;
+}
+
+char * isMnemonic(char * first_str, char * sec_str, char ** opcode_table, int record_len) {
+    // check whether the instruction contains a valid mnemonic
+    if (first_str != NULL && find(first_str, opcode_table, record_len) != "0") {
+	return "Mnemonic";
+    }
+    else if (sec_str != NULL && find(sec_str, opcode_table, record_len) != "0") {
+	return "TEST Mnemonic";
+    }
+    else {
+	return "null";
+    }
+}
+
+int getLenStrArr(char ** s) {
+    // get the length of string array
+    int len = 0;
+    while (s[len] != NULL) {
+	len++;
+    }
+    return len;
+}
+
+int isSymbol(char ** s) {
+    // the way to check this line contains a symbol is to check whether valid string number is 3
+    
+    // get the length of string array
+    int len = getLenStrArr(s);
+
+    int num = 0;
+    for (int i = 0;i < len;i++) {
+	if (s[i] != '\0') {
+	    num++;
+	}
+    }
+    return num == 3;
+}
+
+int isIndexAddressing(char * operand) {
+    // check if operand is index addressing
+    int result = 0;
+    for (int i = 0;i < strlen(operand);i++) {
+	if (operand[i] == ',') {
+	    result = 1;
+	    break;
+	}
+    }
+    return result;
+}
+
+int isInDirectAddresing(char * operand) {
+    if (operand[0] == '@') {
+	return 1;
+    }
+    else {
+	return 0;
+    }
+}
+
+int isImmediateAddresing(char * operand) {
+    if (operand[0] == '#') {
+	return 1;
+    }
+    else {
+	return 0;
+    }
+}
+
+// check if operand is which type of addressing
+void checkAddressingType(char * operand) {
+    if (isIndexAddressing(operand)) {
+        printf("This is index addressing\n");
+    }
+    else if (isInDirectAddresing(operand)) {
+        printf("This is indirect addressing\n");
+    }
+    else if (isImmediateAddresing(operand)) {
+        printf("This is immediate addressing\n");
+    }
+    else {
+	printf("This is direct addressing\n");
+    }
+}
+
+void insertValueInHashTable(char * str, char ** hash_table, int size) {
+    // find a empty space in hash table to place the value
+
+        int val = getCodeOpcode(str);
+        int index = val % size;
+        //printf("%s, %d\n", all_str[i], index);
+        if (hash_table[index] == NULL) { // the bucket not have value
+            hash_table[index] = str;
+        }
+        else { // probing other empty bucket
+            // linear probing the empty bucket
+            int empty_bucket = -1;
+            for (int i = 0;i < size;i++) {
+                if (hash_table[i] == NULL) {
+                    empty_bucket = i;
+                    break;
+                }
+            }
+
+            // check whether have empty bucket
+            if (empty_bucket == -1) {
+                printf("no more empty bucket %s\n", str);
+            }
+            else {
+                hash_table[empty_bucket] = str;
+            }
+        }
+}
+
+void printSymbolTable(char ** symbol_table, int record_len) {
+    printf("Symbol Table : {");
+    int first_symbol = 1;
+	for (int i = 0;i < record_len;i++) {
+	    if (symbol_table[i] != NULL) {
+		char ** str = split(symbol_table[i]);
+		char * symbol = str[0];
+		int address = atoi(str[1]);
+		// print a comma at the end of each symbol if not the last symbol
+		if (first_symbol) {
+	            printf("'%s' : '%x'", symbol, address);
+		    first_symbol = 0;
+		}
+		else {
+	            printf(", '%s' : '%x'", symbol, address);
+		}
+	    }
+        }
+    printf("}\n");
+}
+
+char ** mkSymbolTable(char ** all_str, int record_len, char ** opcode_table, int opcode_table_record_len) {
+    // make the string array needed to put into symbol table
+    char ** symbol_str_arr = calloc(record_len, sizeof(char *));
+    char ** symbol_table = hashTable("", 0, record_len);
+
+    int is_started = 0;
+    for (int i = 0;i < record_len;i++) {
+	char * line = all_str[i];
+	line = commaRemoveSpace(line); // remove a space behind the comma 
+	printf("\n%d : %s\n", i+1, line);
+	line = removeComment(line); // remove comment, which is behind the dot
+
+	// check this line is belong to normal instruction or basic function to use different way to locate
+	int basic_instruction = isBasic(line);
+	int address;
+	//printf("bi is %d\n", basic_instruction);
+	if (basic_instruction == 1) {
+	    // START
+	    is_started = 1;
+	    char ** s_arr = split(line);
+	    address = (int)strtol(s_arr[2], NULL, 16);
+	    printf("Program name is %s\n", s_arr[0]);
+	    printf("start from this line\n\n");
+	}
+	else {
+	    if (basic_instruction) {
+	        if (!is_started) {
+		    printf("error : basic instruction before the program start");
+	        }
+	    	else {
+	            char ** s_arr = split(line);
+		    if (isSymbol(s_arr)) {
+		        // print the address of symbol
+			char * symbol_name = s_arr[0];
+			// convert int adddress to string
+			int address_length = (int)((ceil(log10(address))+1)*sizeof(char)); // length of int
+			char str_address[address_length];
+			sprintf(str_address, "%d", address); // convert int adddress to string
+			// concat the symbol name and a space and address
+			char * temp_symbol_name = strdup(symbol_name);
+			char * hash_value = strcat(temp_symbol_name, " ");
+			hash_value = strcat(temp_symbol_name, str_address);
+			// insert into symbol table
+			insertValueInHashTable(hash_value, symbol_table, record_len);
+		    }
+
+	    	    if (basic_instruction == 2) {
+			// END
+			printf("END of the program\n");
+		    }
+		    else if (basic_instruction == 3) {
+			// WORD
+			printf("WORD is pesudo instruction code\n");
+			int space = 3;
+		    	address += space;
+		    }
+		    else if (basic_instruction == 4) {
+		        // RESW
+			printf("RESW is pesudo instruction code\n");
+			int space = atoi(s_arr[2]) * 3;
+		    	address += space;
+	    	    }
+		    else if (basic_instruction == 5) {
+			// BYTE
+			printf("BYTE is pesudo instruction code\n");
+			int space;
+			if (s_arr[2][0] == 'C') {
+			    // char
+			    // the nums of char
+			    int char_nums = strlen(s_arr[2])-3; // minus the two single quotation and one type
+			    space = char_nums;
+			}
+			else if (s_arr[2][0] == 'X') {
+			    // int
+			    space = 1;
+			}
+		    	address += space;
+		    }
+		    else if (basic_instruction == 6) {
+		    	// RESB
+			printf("RESB is pesudo instruction code\n");
+			int space = atoi(s_arr[2]);
+		    	address += space;
+	    	   }
+	        }	
+	    }
+	    else {
+	    	// not the basic instruction
+	    	if (is_started) {
+		    // check whether it is in opcode table
+	    	    char ** s_arr = split(line);
+		    char * mnemonic = isMnemonic(s_arr[0], s_arr[1], opcode_table, opcode_table_record_len);
+		    if (mnemonic != "null") {
+			int operand_index = getLenStrArr(s_arr)-1; // operand is the last index of a line
+			char * operand = "";
+			if (operand_index != 0) {
+			    // operand is not the only string in line
+			    operand = s_arr[operand_index]; // operand is the last index of a line
+			}
+			// check if operand is which type of addressing
+			checkAddressingType(operand);
+
+			// check if mnemonic is test or normal
+			if (mnemonic == "Mnemonic") {
+			    printf("Label : Mnemonic: %s, operand : %s\n", s_arr[0], operand);
+			}
+			else {
+			    // TEST Mnemonic
+			    printf("Label : TEST Mnemonic: %s, operand : %s\n", s_arr[1], operand);
+		            // print the address of symbol
+			    char * symbol_name = s_arr[0];
+			    // convert int adddress to string
+			    int address_length = (int)((ceil(log10(address))+1)*sizeof(char)); // length of int
+			    char str_address[address_length];
+			    sprintf(str_address, "%d", address); // convert int adddress to string
+			    // concat the symbol name and a space and address
+			    char * temp_symbol_name = strdup(symbol_name);
+			    char * hash_value = strcat(temp_symbol_name, " ");
+			    hash_value = strcat(temp_symbol_name, str_address);
+			    // insert into symbol table
+			    insertValueInHashTable(hash_value, symbol_table, record_len);
+			}
+		        // normal instruction take 3B
+		    	address += 3;
+		    }
+	    	}
+	    }
+	}
+	/*
+	*/
+    }
+    return symbol_table;
+}
+
+char ** mkOpcodeTable() {
+    // find relative opcode and output
+    char * filename = "./opcode.txt";
+    int record_len = recordLen(filename);
+    char ** all_str = readFile(filename);
+
+    char ** hash_table = hashTable(all_str, record_len, record_len); // make a hash table with data in opcode.txt
+    return hash_table;
+}
+
+int main() {
+    char * filename = "./testprog1.S"; // program name
+    int record_len = recordLen(filename);
+    char ** all_str = readFile(filename);
+    // make opcode table
+    int opcode_table_record_len = recordLen("opcode.txt");
+    char ** opcode_table = mkOpcodeTable();
+    char ** symbol_table = mkSymbolTable(all_str, record_len, opcode_table, opcode_table_record_len);
+    printSymbolTable(symbol_table, record_len);
+    //char * a = "ADD";
+    //char s[3] = "ADD";
+    //printf("len %d %d %d\n", strlen(s), strlen("ADD"), strlen(a));
+    //find(s);
+}
---
+++ search_opcode.c
@@ -0,0 +1,183 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int recordLen(char * filename) {
+    // get record nums in opcode.txt
+    FILE* ptr;
+    char str[50];
+    ptr = fopen(filename, "r");
+ 
+    if (NULL == ptr) {
+        printf("file can't be opened \n");
+    }
+ 
+    int record_len = 0;
+ 
+    // counting the length of record 
+    while (fgets(str, 50, ptr) != NULL) {
+        record_len++;
+    }
+    fclose(ptr);
+
+    return record_len;
+}
+
+char ** readFile(char * filename)
+{
+    FILE* ptr;
+    char str[50];
+    ptr = fopen(filename, "r");
+    int index = 0;
+    int record_len = recordLen(filename);
+    char ** all_str = calloc(record_len, sizeof(char *));
+
+    // storing each record
+    while (fgets(str, 50, ptr) != NULL) {
+        all_str[index++] = strdup(str);
+    }
+
+    fclose(ptr);
+    return all_str;
+}
+
+int getCodeOpcode(char * c) {
+    int code = 0;
+    for (int i = 0;i < strlen(c);i++) {
+        if (c[i] == ' ') {
+            break;
+        }
+        code += (int)c[i];
+    }
+    return code;
+}
+
+char ** hashTable(char ** all_str, int record_len, int size) {
+    // make and return a hash table
+    char ** hash_table = calloc(size, sizeof(char*));
+    hash_table[0] = NULL;
+    hash_table[1] = NULL;
+
+    for (int i = 0;i < record_len;i++) {
+        int val = getCodeOpcode(all_str[i]);
+        int index = val % record_len;
+        //printf("%s, %d\n", all_str[i], index);
+        if (hash_table[index] == NULL) { // the bucket not have value
+            hash_table[index] = all_str[i];
+        }
+        else { // probing other empty bucket
+            // linear probing the empty bucket
+            int empty_bucket = -1;
+            for (int i = 0;i < record_len;i++) {
+                if (hash_table[i] == NULL) {
+                    empty_bucket = i;
+                    break;
+                }
+            }
+
+            // check whether have empty bucket
+            if (empty_bucket == -1) {
+                printf("no more empty bucket %s\n", all_str[i]);
+            }
+            else {
+                hash_table[empty_bucket] = all_str[i];
+            }
+        }
+    }
+
+    return hash_table;
+}
+
+int cmpMnemonic(char * str, char * c, int record_len) {
+    for (int i = 0;i < record_len;i++) {
+        if (str[i] == ' ') {
+            if (i != strlen(c)) { // not same length, then not same string
+                // minus a char of new line
+	    	//printf("%s and %s are not the same string, as length different %s %s\n", str, c, strlen(str), strlen(c));
+	    	//printf("%s and %s are not the same string, as length different, %d %d\n", str, c, strlen(c), i);
+                return 1; // not same string
+            }
+            break;
+        }
+        if (str[i] != c[i]) {
+	    //printf("%s and %s are not the same string, as the char different %c %c\n", str, c, str[i], c[i]);
+            return 1; // not same string
+        }
+    }
+    //printf("%s and %s are same string\n", str, c);
+    return 0; // str and c are same string
+}
+
+char * getOpcode(char * c) { // get opcode part of each line
+    char * opcode = malloc(strlen(c) * sizeof(char));
+    int index = 0;
+    int start_opcode = 0;
+    char * temp = malloc(sizeof(char));
+    for (int i = 0;i < strlen(c);i++) {
+        if (c[i] == ' ') {
+            start_opcode = 1;
+            continue;
+        }
+        if (start_opcode) {
+            temp[0] = c[i]; // temp is a char pointer to temp store the char
+            opcode[index++] = c[i];
+        }
+    }
+    for (int i = index;i < strlen(opcode);i++) {
+        opcode[i] = 0;
+    }
+    return opcode;
+}
+
+int getCode(char * c) {
+    int code = 0;
+    // last char is new line, so ignore it
+    for (int i = 0;i < strlen(c)-1;i++) {
+        code += (int)c[i];
+    }
+    return code;
+}
+
+char * find(char * mnemonic, char ** hash_table, int record_len) {
+    int mnemonic_index = getCode(mnemonic);
+    //int record_len = sizeof(hash_table) / sizeof(hash_table[0]);
+    mnemonic_index = mnemonic_index % record_len;
+    int temp_mnemonic_index = mnemonic_index;
+
+    while (cmpMnemonic(hash_table[mnemonic_index], mnemonic, record_len)) {
+	//printf("hash value : %s %s\n", hash_table[mnemonic_index], mnemonic);
+        mnemonic_index++;
+        if (mnemonic_index == temp_mnemonic_index) {
+            // stop finding when back to start index
+            //printf("can not found the opcode of %s\n", mnemonic);
+            //printf("invalid\n");
+            return "0";
+        }
+        if (mnemonic_index >= record_len) {
+            // find from 0
+            mnemonic_index = 0;
+        }
+    }
+
+    // output
+    //printf("the relative opcode of %s = %s", mnemonic, getOpcode(hash_table[mnemonic_index]));     
+    char * opcode = getOpcode(hash_table[mnemonic_index]);
+    //printf("opCode : %s\n", opcode);
+    return opcode;
+}
+
+/*
+int main() {
+
+    // input menmonic
+    //char mnemonic[20];
+    //printf("search (Input a mnemonic) : ");
+    //fgets(mnemonic, 20, stdin);
+
+    // find relative opcode and output
+    char s[3] = "ADD";
+    find(s);
+
+    return 0;
+}
+*/
